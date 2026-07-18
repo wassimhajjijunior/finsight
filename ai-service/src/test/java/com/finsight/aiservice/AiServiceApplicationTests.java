f472990695320b4276257d0da2ba8a84b123fbe7
wassimhajjijunior <wassim.hajji@supcom.tn>
2026-07-18 15:34:09 +0100

    first commit

---
+++ ai-service/src/test/java/com/finsight/aiservice/AiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finsight.aiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
